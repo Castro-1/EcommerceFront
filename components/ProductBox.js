8665d551d5b23e79adcb6cf7c63ff2d73398618e
Castro-1 <jecastrog@eafit.edu.co>
2023-06-18 11:03:27 -0500

    latest products done

---
+++ components/ProductBox.js
@@ -0,0 +1,71 @@
+import { styled } from "styled-components";
+import Button from "./Button";
+import Link from "next/link";
+
+const ProductWrapper = styled.div``;
+
+const WhiteBox = styled(Link)`
+  background-color: #fff;
+  padding: 20px;
+  height: 120px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  img {
+    max-width: 100%;
+    max-height: 80px;
+  }
+`;
+
+const Title = styled(Link)`
+  font-weight: normal;
+  font-size: 0.9rem;
+  margin: 0;
+  color: inherit;
+  text-decoration: none;
+`;
+
+const ProductInfoBox = styled.div`
+  margin-top: 5px;
+`;
+
+const PriceRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 2px;
+`;
+
+const Price = styled.div`
+  font-size: 1.5rem;
+  font-weight: 600;
+`;
+
+export default function ProductWhiteBox({
+  _id,
+  name,
+  description,
+  price,
+  images,
+}) {
+  const url = "/product/" + _id;
+  return (
+    <ProductWrapper>
+      <WhiteBox href={url}>
+        <div>
+          <img src={images[0]} alt="product image" />
+        </div>
+      </WhiteBox>
+      <ProductInfoBox>
+        <Title href={url}>{name}</Title>
+        <PriceRow>
+          <Price>${price}</Price>
+          <Button primary outline>
+            Add to cart
+          </Button>
+        </PriceRow>
+      </ProductInfoBox>
+    </ProductWrapper>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Header from "@/components/Header";
 import Featured from "@/components/Featured";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
+import NewProducts from "@/components/NewProducts";
 
-export default function Home({ product }) {
+export default function Home({ featuredProduct, newProducts }) {
   return (
     <div>
       <Header />
-      <Featured product={product} />
+      <Featured product={featuredProduct} />
+      <NewProducts products={newProducts} />
     </div>
   );
 }
@@ -15,8 +17,15 @@ export default function Home({ product }) {
 export async function getServerSideProps() {
   const featuredProductId = "648b329d17c2f5174c854cc2";
   await mongooseConnect();
-  const product = await Product.findById(featuredProductId);
+  const featuredProduct = await Product.findById(featuredProductId);
+  const newProducts = await Product.find({}, null, {
+    sort: { _id: -1 },
+    limit: 10,
+  });
   return {
-    props: { product: JSON.parse(JSON.stringify(product)) },
+    props: {
+      featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
+      newProducts: JSON.parse(JSON.stringify(newProducts)),
+    },
   };
 }
